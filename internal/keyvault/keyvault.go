3836d129c7b634951dce18ce3832983e7314796d
olivershang <olivershang0017@gmail.com>
2023-05-30 16:57:20 +0800

    Removed log.fatal
    Signed-off-by: olivershang <olivershang0017@gmail.com>

--- internal/keyvault/keyvault.go
+++ internal/keyvault/keyvault.go
@@ -4,15 +4,14 @@ import (
 	"context"
 	"crypto/x509"
 	"encoding/base64"
+	"errors"
 	"github.com/hashicorp/vault-client-go"
 	"github.com/hashicorp/vault-client-go/schema"
 	"github.com/notaryproject/notation-hashicorp-vault/internal/crypto"
-	"log"
 	"os"
 	"strings"
 	"time"
 )
-
 
 type VaultClientWrapper struct {
 	vaultClient *vault.Client
@@ -23,27 +22,27 @@ type VaultClientWrapper struct {
 func NewVaultClientFromKeyID(id string) (*VaultClientWrapper, error) {
 	// read addr and token from environment variables
 	vaultAddr := os.Getenv("VAULT_ADDR")
-	if len(VAULTADDR) < 1 {
+	if len(vaultAddr) < 1 {
 		return nil, errors.New("failed to load vault address")
 	}
 
 	vaultToken := os.Getenv("VAULT_TOKEN")
-	if len(VAULTTOKEN) < 1 {
-		log.Fatal("Error loading vault token")
+	if len(vaultToken) < 1 {
+		return nil, errors.New("Error loading vault token")
 	}
 
 	// prepare a client with the given base address
 	client, err := vault.New(
-		vault.WithAddress(VAULTADDR),
+		vault.WithAddress(vaultAddr),
 		vault.WithRequestTimeout(30*time.Second),
 	)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	// authenticate with a root token (insecure)
-	if err := client.SetToken(VAULTTOKEN); err != nil {
-		log.Fatal(err)
+	if err := client.SetToken(vaultToken); err != nil {
+		return nil, err
 	}
 
 	return &VaultClientWrapper{
@@ -54,13 +53,13 @@ func NewVaultClientFromKeyID(id string) (*VaultClientWrapper, error) {
 
 func (vw *VaultClientWrapper) GetCertificateChain(ctx context.Context) ([]*x509.Certificate, error) {
 	// read a certChain
-	secret, err := vw.vaultClient.Secrets.KVv2Read(ctx, vw.keyID)
+	secret, err := vw.vaultClient.Secrets.KvV2Read(ctx, vw.keyID)
 	if err != nil {
 		return nil, err
 	}
-        certString, ok := secret.Data.Data["certificate"].(string)
+	certString, ok := secret.Data.Data["certificate"].(string)
 	if !ok {
-	    return nil, errors.New("failed to parse certificate from KV secrets engine")
+		return nil, errors.New("failed to parse certificate from KV secrets engine")
 	}
 	certBytes := []byte(certString)
 	return crypto.ParseCertificates(certBytes)
@@ -82,7 +81,7 @@ func (vw *VaultClientWrapper) SignWithTransit(ctx context.Context, encodedData s
 
 	signature, ok := resp.Data["signature"].(string)
 	if !ok {
-	    return nil, errors.New("failed to parse signature from TransitSign response")
+		return nil, errors.New("failed to parse signature from TransitSign response")
 	}
 	items := strings.Split(signature, ":")
 	sigBytes, err := base64.StdEncoding.DecodeString(items[2])
